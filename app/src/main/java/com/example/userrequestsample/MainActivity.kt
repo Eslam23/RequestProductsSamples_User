7a09575606898e213a4a8d74f33ba625564eb74d
Eslam23 <eslammsaid2391998@gmail.com>
2021-09-29 01:46:45 +0200

    user request sample initial login

---
+++ app/src/main/java/com/example/userrequestsample/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.userrequestsample
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
+    private lateinit var login: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        email = findViewById(R.id.et_email)
+        password = findViewById(R.id.et_pass)
+        login = findViewById(R.id.btn_login)
+
+        login.setOnClickListener {
+            if(email.text.toString().trim().isEmpty()){
+                email.error= "Email is Empty"
+                email.requestFocus()
+            }
+
+            else if (Patterns.EMAIL_ADDRESS.matcher(email.text).matches().not())
+            {
+                email.error = "Enter Correct Format"
+                email.requestFocus()
+
+            }
+            else if(password.text.toString().trim().isEmpty()){
+                password.error = "Password is Empty"
+                password.requestFocus()
+            }
+            else if (password.text.toString().trim().length < 8)
+            {
+                password.error = "Password should be more than 8 digit"
+                password.requestFocus()
+
+            }
+            else
+            {
+                val intent = Intent(applicationContext , HomeActivity::class.java)
+                startActivity(intent)
+
+            }
+        }
+    }
+}
