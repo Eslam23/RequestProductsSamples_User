e98e5a7d036557e2403353dd1eb5e7cee7227f0d
Eslam23 <eslammsaid2391998@gmail.com>
2021-09-29 15:57:21 +0200

    adding splash screen

---
+++ app/src/main/java/com/example/userrequestsample/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.userrequestsample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var logo: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        logo = findViewById(R.id.logo_id)
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this , MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in ,android.R.anim.fade_out )
+            finish()
+        }
+
+    }
+}
